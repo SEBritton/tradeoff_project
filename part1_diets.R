87536de3df3f24e97bd0e280c6b83846b1840b50
SEBritton <sarahebritton3@gmail.com>
2024-01-30 17:02:24 -0700

    Working on models and figures.

--- part1_diets.R
+++ part1_diets.R
@@ -1,11 +1,6 @@
 # The effect of diet on melanin pigmentation in Hyles lineata
 #Authors: Sarah Britton and Goggy Davidowitz
 
-#Installs needed packages
-list.of.packages <- c("ggplot2", "car", "multcomp","extrafont", "ggpubr")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-
 # Libraries
 library(ggplot2) #for making plots
 library(car) #for Anova function
@@ -13,29 +8,32 @@ library(multcomp) #for Tukey tests
 library(extrafont) #for plot fonts
 library(dplyr) #for piping and grouping
 library(ggpubr) #for combining ggplots
+library(emmeans) #for post hoc tests
+library(lmerTest) # for extracting p vals from mixed models
+library(lme4) # mixed models
 
 #Read in Data
-diet_data <- read.csv(file="Diet_Data.csv")
+diet_data <- read.csv(file="data/Diet_Data.csv")
 diet_data$Diet_Treatment <- as.factor(diet_data$Diet_Treatment) #convert to factor
 
-extraction_data<-read.csv(file="Extraction_data.csv")
-diet_data<- merge(diet_data, extraction_data, by=c("ID", "Diet_Treatment")) 
+extraction_data<-read.csv(file="data/Extraction_data.csv")
+extraction_data_full<- merge(diet_data, extraction_data, by=c("ID", "Diet_Treatment")) 
 
-diet_data$Extraction_Run <- as.factor(diet_data$Extraction_Run) #convert to factor
+extraction_data_full$Extraction_Run <- as.factor(extraction_data_full$Extraction_Run) #convert to factor
 
-diet_data$Concentration_PhotoMass<-diet_data$Total_Extracted/diet_data$Photo_Mass
-diet_data$Concentration_BlockingMass<-diet_data$Total_Extracted/diet_data$Blocking_Mass
+extraction_data_full$Concentration_PhotoMass<-extraction_data_full$Total_Extracted/extraction_data_full$Photo_Mass
+extraction_data_full$Concentration_BlockingMass<-extraction_data_full$Total_Extracted/extraction_data_full$Blocking_Mass
 
-feeding_data <- read.csv(file="Feeding_Data.csv")
+feeding_data <- read.csv(file="data/Feeding_Data.csv")
 feeding_data$Diet_Treatment <- as.factor(feeding_data$Diet_Treatment) #convert to factor
 
-diet_data<- merge(diet_data, feeding_data, by=c("ID", "Diet_Treatment")) 
+feeding_data<- merge(diet_data, feeding_data, by=c("ID", "Diet_Treatment")) 
 
 
 #Check distribution of  response variables
 hist(diet_data$Melanin_Percent)
 hist(diet_data$Darkness)
-hist(extraction_data$Concentration_General)
+hist(extraction_data_full$Concentration_PhotoMass)
 
 
 #These lists will be used for making plots for ImageJ data
@@ -53,9 +51,8 @@ diet_data_med <- subset(diet_data, Diet_Treatment=='MEDIUM')
 diet_data_low2 <- subset(diet_data, Diet_Treatment=='LOW-2')
 diet_data_high2 <- subset(diet_data, Diet_Treatment=='HIGH-2')
 
-extraction_data_low <- subset(extraction_data, Diet_Treatment=='LOW')
-extraction_data_high <- subset(extraction_data, Diet_Treatment=='HIGH')
-extraction_data_high_nooutliers<-extraction_data_high[-c(17,34), ]
+extraction_data_low <- subset(extraction_data_full, Diet_Treatment=='LOW')
+extraction_data_high <- subset(extraction_data_full, Diet_Treatment=='HIGH')
 
 
 
@@ -67,7 +64,7 @@ area_graph<-ggplot(diet_data, aes(x=factor(Diet_Treatment, level = diet_order), 
   stat_summary(fun.y="mean", aes(color=Diet_Treatment), shape=18, size=1) + #add mean value points to graph (as diamonds)
   scale_color_manual(values=diet_colors) + #chooses colors for reach diet treatment (from above)
   annotate(geom="text", x=c(1,2,3,4,5), y=c(86,88,92,90,89),label=c("a", "ab", "b", "b", "b")) + #adds sig letter labels
-  annotate(geom="text", x=4.5, y=95, label="F=4.48, p=0.002")+
+  annotate(geom="text", x=4.5, y=95, label="n=258, p=0.002")+
   theme_classic(base_size = 16)+ theme(legend.position="none",text=element_text(family="Times New Roman")) + #text font and size, remove legend
   xlab("Diet Treatment") + ylab("Percent Area") #axis labels
 
@@ -79,7 +76,7 @@ darkness_graph<-ggplot(diet_data, aes(x=factor(Diet_Treatment, level = diet_orde
   stat_summary(fun.y="mean", aes(color=Diet_Treatment), shape=18, size=1) +
   scale_color_manual(values=diet_colors) + 
   annotate(geom="text", x=c(1,2,3,4,5), y=c(26,26.8,26,26.7,27.5 ),label=c("a", "ab", "a", "b", "b")) +
-  annotate(geom="text", x=4.5, y=28.2, label="F=6.59, p<0.001")+
+  annotate(geom="text", x=4.5, y=28.2, label="n=258, p<0.001")+
   theme_classic(base_size = 16) + theme(legend.position="none",text=element_text(family="Times New Roman")) + 
   xlab("Diet Treatment") + ylab("Darkness")
 
@@ -99,7 +96,7 @@ size_graph<-ggplot(diet_data, aes(x=factor(Diet_Treatment, level = diet_order), 
   stat_summary(fun.y="mean", aes(color=Diet_Treatment), shape=18, size=1) +
   scale_color_manual(values=diet_colors) +  
   annotate(geom="text", x=c(1,2,3,4,5), y=c(4.6,3.3,5.7,3.5,4),label=c("a", "c", "a", "bc", "ab")) + 
-  annotate(geom="text", x=4.5, y=5.3, label="F=11.83, p<0.001")+
+  annotate(geom="text", x=4.5, y=5.3, label="n=258, p<0.001")+
   theme_classic(base_size = 16) + theme(legend.position="none",text=element_text(family="Times New Roman")) +
   xlab("Diet Treatment") + ylab("Mass (g)")
 
@@ -139,28 +136,28 @@ ggplot(diet_data, aes(Melanin_Percent, Darkness))+ geom_point(aes(color=Diet_Tre
 
 
 #Figures- Extraction Results 
-ggplot(extraction_data, aes(x=factor(Diet_Treatment, level=diet_order_extract), Concentration_PhotoMass)) +
+ggplot(extraction_data_full, aes(x=factor(Diet_Treatment, level=diet_order_extract), Concentration_PhotoMass)) +
   geom_boxplot (aes(color=Diet_Treatment), size=1)+
   stat_summary(fun.y="mean", aes(color=Diet_Treatment), shape=18, size=1) +
   scale_color_manual(values=diet_colors_extract) +
   geom_signif(comparison=list(c("LOW","HIGH")), map_signif_level = TRUE)+
-  annotate(geom="text", x=2.3, y=100, label="t(77)=2.83,")+
-  annotate(geom="text", x=2.3, y=90, label="p=0.006")+
+  annotate(geom="text", x=2.4, y=150, label="t(72)=2.87,")+
+  annotate(geom="text", x=2.4, y=140, label="p=0.005")+
   theme_classic(base_size = 16) + theme(legend.position="none",text=element_text(family="Times New Roman")) +
   xlab("Diet Treatment") + ylab("Melanin Concentration (ug melanin/g body mass)")
 
 
-ggplot(extraction_data, aes(Concentration_PhotoMass, Melanin_Percent))+ geom_point(aes(color=Diet_Treatment), size=1)+ 
-  geom_smooth(aes(color=Diet_Treatment), method=lm, se=FALSE) + 
+ggplot(extraction_data_full, aes(Concentration_PhotoMass, Melanin_Percent))+ geom_point(aes(color=Diet_Treatment), size=1)+ 
+  geom_smooth(aes(color=Diet_Treatment), method=lm, se=TRUE, fill="gray80") + 
   scale_color_manual(values=diet_colors_extract) +
   theme_classic(base_size = 16) + theme(text=element_text(family="Times New Roman")) +
-  xlab("Concentration") + ylab("Percent Area")
+  xlab("Melanin Concentration (ug melanin/g body mass)") + ylab("Percent Area Melanin")
 
-ggplot(extraction_data, aes(Concentration_PhotoMass, Darkness))+ geom_point(aes(color=Diet_Treatment), size=1)+ 
-  geom_smooth(aes(color=Diet_Treatment), method=lm, se=FALSE) + 
+ggplot(extraction_data_full, aes(Concentration_PhotoMass, Darkness))+ geom_point(aes(color=Diet_Treatment), size=1)+ 
+  geom_smooth(aes(color=Diet_Treatment), method=lm, se=TRUE, fill="gray80") + 
   scale_color_manual(values=diet_colors_extract) +
   theme_classic(base_size = 16) + theme(text=element_text(family="Times New Roman")) +
-  xlab("Concentration") + ylab("Darkness")
+  xlab("Melanin Concentration (ug melanin/g body mass)") + ylab("Darkness")
 
 
 
@@ -182,35 +179,28 @@ diet_data %>%
   as.data.frame
 
 #Melanin Percent
-Percent_Area_Test<-aov(Melanin_Percent ~ Diet_Treatment, data = diet_data)
-Anova(Percent_Area_Test, type = 2) #Shows a type 2 anova (no interaction)
-Percent_Area_Tukey <- TukeyHSD(Percent_Area_Test) #post hoc test
-Percent_Area_Tukey
+Percent_Melanin_mod <- lm(Melanin_Percent ~ Diet_Treatment, data=diet_data)
+summary(Percent_Melanin_mod)
+emmeans(Percent_Melanin_mod, specs="Diet_Treatment") |> pairs(adjust="tukey")
+qqnorm(residuals(Percent_Melanin_mod)) #normality of residuals
+plot(fitted(Percent_Melanin_mod), residuals(Percent_Melanin_mod)) #homoskedasticity 
 
-#Check assumptions of model 
-qqnorm(resid(Percent_Area_Test)) #normality of residuals
-plot(fitted(Percent_Area_Test),residuals(Percent_Area_Test)) #homoskedasticity 
 
 #Darkness
-Darkness_Test<-aov(Darkness ~ Diet_Treatment, data = diet_data)
-Anova(Darkness_Test, type = 2)
-Darkness_Tukey <- TukeyHSD(Darkness_Test)
-Darkness_Tukey
-
-#Check assumptions of model 
-qqnorm(resid(Darkness_Test)) #normality of residuals
-plot(fitted(Darkness_Test),residuals(Darkness_Test)) #homoskedasticity
+Darkness_mod <- lm(Darkness ~ Diet_Treatment, data=diet_data)
+summary(Darkness_mod)
+emmeans(Darkness_mod, specs="Diet_Treatment") |> pairs(adjust="tukey")
+qqnorm(residuals(Darkness_mod)) #normality of residuals
+plot(fitted(Darkness_mod), residuals(Darkness_mod)) #homoskedasticity 
 
 
 #Size
-Size_Test<-aov(Photo_Mass ~ Diet_Treatment, data = diet_data)
-Anova(Size_Test, type = 2)
-Photo_Size_Tukey <- TukeyHSD(Size_Test)
+Size_mod<-lm(Photo_Mass ~ Diet_Treatment, data = diet_data)
+summary(Size_mod)
+emmeans(Size_mod, specs="Diet_Treatment") |> pairs(adjust="tukey")
 Photo_Size_Tukey
-
-#Check assumptions of model 
-qqnorm(resid(Size_Test)) #normality of residuals
-plot(fitted(Size_Test),residuals(Size_Test)) #homoskedasticity
+qqnorm(residuals(Size_mod)) #normality of residuals
+plot(fitted(Size_mod),residuals(Size_mod)) #homoskedasticity
 
 
 Size_Regress<-lm(Melanin_Percent ~ Photo_Mass+Diet_Treatment+Photo_Mass*Diet_Treatment, data = diet_data)
@@ -223,13 +213,10 @@ summary(Size_Regress_2)
 
 
 #Statistics- Extraction Results
-Concentration_Test<-aov(Concentration_PhotoMass ~ Diet_Treatment, data = extraction_data)
-Anova(Concentration_Test, type = 2)
-
-t.test(extraction_data$Concentration_PhotoMass~extraction_data$Diet_Treatment)
+t.test(extraction_data_full$Concentration_PhotoMass~extraction_data_full$Diet_Treatment)
 
 
-Extract1 <-lm(Melanin_Percent ~ Concentration_PhotoMass+Diet_Treatment+Concentration_PhotoMass:Diet_Treatment, data=extraction_data)
+Extract1 <-lm(Melanin_Percent ~ Concentration_PhotoMass+Diet_Treatment+Concentration_PhotoMass:Diet_Treatment, data=extraction_data_full)
 summary(Extract1)
 
 Extract1a <-lm(Melanin_Percent ~ Concentration_PhotoMass, data=extraction_data_low)
@@ -238,7 +225,7 @@ summary(Extract1a)
 Extract1b <-lm(Melanin_Percent ~ Concentration_PhotoMass, data=extraction_data_high)
 summary(Extract1b)
 
-Extract2 <-lm(Darkness ~ Concentration_PhotoMass+Diet_Treatment+Concentration_PhotoMass:Diet_Treatment, data=extraction_data)
+Extract2 <-lm(Darkness ~ Concentration_PhotoMass+Diet_Treatment+Concentration_PhotoMass:Diet_Treatment, data=extraction_data_full)
 summary(Extract2)
 
 Extract2a <-lm(Darkness ~ Concentration_PhotoMass, data=extraction_data_low)
@@ -247,8 +234,6 @@ summary(Extract2a)
 Extract2b <-lm(Darkness ~ Concentration_PhotoMass, data=extraction_data_high)
 summary(Extract2b)
 
-Extract2c <-lm(Darkness ~ Concentration_PhotoMass, data=extraction_data_high_nooutliers)
-summary(Extract2c)
 
 
 
@@ -269,7 +254,7 @@ Size_Regress_med <-lm(Melanin_Percent~ Photo_Mass, data=diet_data_med)
 summary(Size_Regress_med)
 
 
-##Feedinig Data
+##Feeding Data
 feeding_graph<-ggplot(feeding_data, aes(x=factor(Diet_Treatment, level = diet_order))) +
   geom_boxplot(aes(y=fourth_food_change, color=Diet_Treatment), size=1) + 
   stat_summary(fun.y="mean", aes(y=fourth_food_change, color=Diet_Treatment), shape=18, size=1) +
@@ -277,9 +262,9 @@ feeding_graph<-ggplot(feeding_data, aes(x=factor(Diet_Treatment, level = diet_or
   stat_summary(fun.y="mean", aes(y=fifth_food_change, color=Diet_Treatment), shape=18, size=1) +
   scale_color_manual(values=diet_colors) + 
   annotate(geom="text", x=c(1,2,3,4,5), y=c(1750,1380,2050,1220,1190),label=c("a", "b", "a", "b", "b")) +
-  annotate(geom="text", x=c(1,2,3,4,5), y=c(500, 430, 650, 400, 490),label=c("ab", "bc", "a", "c", "bc")) +
-  annotate(geom="text", x=4.5, y=2000,label="5th: F=25.48, p<0.001") +
-  annotate(geom="text", x=4.5, y=1800,label="4th: F=5.43, p<0.001") +
+  annotate(geom="text", x=c(1,2,3,4,5), y=c(500, 430, 650, 400, 490),label=c("cd", "de", "c", "e", "dc")) +
+  annotate(geom="text", x=4.5, y=2000,label="5th: n=108, p<0.001") +
+  annotate(geom="text", x=4.5, y=1800,label="4th: n=108, p<0.001") +
   theme_classic(base_size = 16)+ theme(legend.position="none",text=element_text(family="Times New Roman")) + 
   xlab("Diet Treatment") + ylab("Change in food (mg)")
 
