087930c73651068952a0bab231348311ba47b5db
SEBritton <sarahebritton3@gmail.com>
2024-09-27 09:56:10 -0700

    Cleaning up scripts, updating plots

--- part1_diets.R
+++ part1_diets.R
@@ -16,99 +16,112 @@ library(dplyr) #for piping and grouping
 
 #Read in data, convert variables and combine data sets as needed
 diet_data <- read.csv(file="data/diet_data.csv")
-diet_data$diet_treatment <- as.factor(diet_data$diet_treatment) #convert to factor
-extraction_data<-read.csv(file="data/Extraction_data.csv")
-extraction_data_full<- merge(diet_data, extraction_data, by=c("ID", "diet_treatment")) 
-extraction_data_full$extraction_run <- as.factor(extraction_data_full$extraction_run) #convert to factor
-extraction_data_full$concentration_photo_mass<-extraction_data_full$total_extracted/extraction_data_full$photo_mass
-extraction_data_full$concentration_blocking_mass<-extraction_data_full$total_extracted/extraction_data_full$blocking_mass
+diet_data$diet_treatment <- as.factor(diet_data$diet_treatment) 
 
+extraction_data<-read.csv(file="data/Extraction_data.csv")
+extraction_data$diet_treatment <- as.factor(extraction_data$diet_treatment) 
+
+extraction_data_full<- merge(extraction_data, diet_data, by=c("ID", "diet_treatment")) 
+
+extraction_data_full<-extraction_data_full |>
+  mutate(extraction_run=as.factor(extraction_run)) |>
+  mutate(concentration_photo_mass = total_extracted/photo_mass) |>
+  mutate(log_concentration = log10(concentration_photo_mass )) 
+
+feeding_data <- read.csv(file="data/Feeding_Data.csv")
+feeding_data$diet_treatment <- as.factor(feeding_data$diet_treatment) 
+
+  
 #Check distribution of  response variables
-hist(diet_data$avg_percent)
+hist(diet_data$melanin_percent)
 hist(diet_data$darkness)
 hist(extraction_data_full$concentration_photo_mass)
+hist(extraction_data_full$log_concentration)
 
 
-#These lists will be used for making plots for ImageJ data
-diet_colors = c("orange", "orange3", "green2", "green4", "yellow") #assigns colors to each diet treatment for figures
-diet_order = c('LOW', 'MEDIUM', 'HIGH', 'LOW-2', 'HIGH-2') #puts diet in an order that makes sense for figures
+#These lists will be used for making plots for ImageJ data (color for presentations)
+diet_colors = c("orange", "orange3", "green2", "green4", "yellow") 
+diet_order = c('LOW', 'MEDIUM', 'HIGH', 'LOW-2', 'HIGH-2') 
 diet_labels=c('Low-P/\nLow-T', 'Low-P/\nMed-T', 'Low-P/\nHigh-T', 'High-P/\nLow-T', 'High-P/\nHigh-T')
 
-#These lists will be used for making plots for extraction data
-diet_order_extract = c('LOW','HIGH') #puts diet in an order that makes sense for figures
+#These lists will be used for making plots for extraction data for presentations (color for presentations)
+diet_order_extract = c('LOW','HIGH') 
 diet_labels_extract=c('Low-P/\nLow-T','Low-P/\nHigh-T')
-diet_colors_extract = c("orange", "green2") #assigns colors to each diet treatment for figures
+diet_colors_extract = c("orange", "green2")
 
 ####Statistics####
-#Create table of descriptive stats (mean and sd) for all 4 treatment groups
+#Create table of descriptive stats (mean and sd) for treatment groups
 diet_data %>%
   group_by(diet_treatment) %>%
-  summarize(area_mean = mean(avg_percent, na.rm = TRUE),
-            area_sd = sd(avg_percent, na.rm = TRUE),
-            area_median = median(avg_percent, na.rm = TRUE),
+  summarize(area_mean = mean(melanin_percent, na.rm = TRUE),
+            area_sd = sd(melanin_percent, na.rm = TRUE),
             darkness_mean = mean(darkness, na.rm = TRUE),
             darkness_sd = sd(darkness, na.rm = TRUE),
-            darkness_median = median(darkness, na.rm = TRUE),
             size_mean = mean(photo_mass, na.rm = TRUE),
-            size_sd = sd(photo_mass, na.rm = TRUE),
-            size_median = median(photo_mass, na.rm = TRUE),
-            fourth_mean = mean(X4th_food_change, na.rm = TRUE),
-            fourth_sd = sd(X4th_food_change, na.rm = TRUE),
-            fourth_median = median(X4th_food_change, na.rm = TRUE),
-            fifth_mean = mean(X5th_food_change, na.rm = TRUE),
-            fifth_sd = sd(X5th_food_change, na.rm = TRUE),
-            fifth_median = median(X5th_food_change, na.rm = TRUE)) %>%
+            size_sd = sd(photo_mass, na.rm = TRUE))
   as.data.frame
+  
+feeding_data %>%
+  group_by(diet_treatment) %>%
+  summarize(fourth_mean = mean(fourth_food_change, na.rm = TRUE),
+            fourth_sd = sd(fourth_food_change, na.rm = TRUE),
+            fifth_mean = mean(fifth_food_change, na.rm = TRUE),
+            fifth_sd = sd(fifth_food_change, na.rm = TRUE))
+  as.data.frame
+  
 
 #Percent melanin
-percent_mod <- lm(avg_percent ~ diet_treatment, data=diet_data)
+percent_mod <- lm(melanin_percent ~ diet_treatment, data=diet_data)
 summary(percent_mod)
 emmeans(percent_mod, specs="diet_treatment") |> pairs(adjust="tukey")
-qqnorm(residuals(percent_mod)) #normality of residuals
-plot(fitted(proportion_mod), residuals(proportion_mod)) #homoskedasticity 
+qqnorm(residuals(percent_mod)) 
+plot(fitted(percent_mod), residuals(percent_mod)) 
 
 #Darkness
 darkness_mod <- lm(darkness ~ diet_treatment, data=diet_data)
 summary(darkness_mod)
 emmeans(darkness_mod, specs="diet_treatment") |> pairs(adjust="tukey")
-qqnorm(residuals(darkness_mod)) #normality of residuals
-plot(fitted(darkness_mod), residuals(darkness_mod)) #homoskedasticity 
+qqnorm(residuals(darkness_mod))
+plot(fitted(darkness_mod), residuals(darkness_mod)) 
 
 #Size
 size_mod<-lm(photo_mass ~ diet_treatment, data = diet_data)
 summary(size_mod)
 emmeans(size_mod, specs="diet_treatment") |> pairs(adjust="tukey")
-qqnorm(residuals(size_mod)) #normality of residuals
-plot(fitted(size_mod),residuals(size_mod)) #homoskedasticity
+qqnorm(residuals(size_mod)) 
+plot(fitted(size_mod),residuals(size_mod))
 
-size_regress<-lm(avg_percent ~ photo_mass, data = diet_data)
+size_regress<-lm(melanin_percent ~ photo_mass, data = diet_data)
 summary(size_regress)
 
 size_regress2<-lm(darkness ~ photo_mass, data = diet_data)
 summary(size_regress2)
 
 #Feeding data
-Feeding_Test_4<-lm(X4th_food_change ~ diet_treatment, data = diet_data)
-summary(Feeding_Test_4)
-emmeans(Feeding_Test_4, specs="diet_treatment") |> pairs(adjust="tukey")
-qqnorm(residuals(Feeding_Test_4))
-plot(fitted(Feeding_Test_4),residuals(Feeding_Test_4)) #homoskedasticity
+feeding_test_4<-lm(fourth_food_change ~ diet_treatment, data = feeding_data)
+summary(feeding_test_4)
+emmeans(feeding_test_4, specs="diet_treatment") |> pairs(adjust="tukey")
+qqnorm(residuals(feeding_test_4))
+plot(fitted(feeding_test_4),residuals(feeding_test_4)) 
 
-Feeding_Test_5<-lm(X5th_food_change ~ diet_treatment, data = diet_data)
-summary(Feeding_Test_5)
-emmeans(Feeding_Test_5, specs="diet_treatment") |> pairs(adjust="tukey")
-qqnorm(residuals(Feeding_Test_5))
-plot(fitted(Feeding_Test_5),residuals(Feeding_Test_5)) #homoskedasticity
+feeding_test_5<-lm(fifth_food_change ~ diet_treatment, data = feeding_data)
+summary(feeding_test_5)
+emmeans(feeding_test_5, specs="diet_treatment") |> pairs(adjust="tukey")
+qqnorm(residuals(feeding_test_5))
+plot(fitted(feeding_test_5),residuals(feeding_test_5)) 
 
 #Extraction data
 t.test(extraction_data_full$concentration_photo_mass~extraction_data_full$diet_treatment)
 
-extraction_test <- lm(concentration_photo_mass ~ avg_percent*darkness, data=extraction_data_full)
+t.test(extraction_data_full$log_concentration~extraction_data_full$diet_treatment)
+
+extraction_test <- lm(concentration_photo_mass ~ darkness, data=extraction_data_full)
 summary(extraction_test)
+
 
 ####Figures for Paper####
 #Figure 2
-area_graph_paper<-ggplot(diet_data, aes(x=factor(diet_treatment, level = diet_order),  avg_percent)) + 
+area_graph_paper<-ggplot(diet_data, aes(x=factor(diet_treatment, level = diet_order),  melanin_percent)) + 
   geom_boxplot(outlier.shape=NA) +
   geom_jitter(width=0.17, alpha=0.5)+
   stat_summary(fun ="mean", shape=18, size=1.2) + 
@@ -155,7 +168,7 @@ size_graph_paper<-ggplot(diet_data, aes(x=factor(diet_treatment, level = diet_or
   xlab("Diet Treatment") + ylab("Mass (g)")
 size_graph_paper
 
-feeding_4th_paper <- ggplot(diet_data, aes(x=factor(diet_treatment, level = diet_order), y=X4th_food_change)) +
+feeding_4th_paper <- ggplot(feeding_data, aes(x=factor(diet_treatment, level = diet_order), y=fourth_food_change)) +
   geom_boxplot(outlier.shape = NA) + 
   geom_jitter(width=0.13, alpha=0.5) +
   stat_summary(fun.y="mean", shape=18, size=1.2) +
@@ -165,7 +178,7 @@ feeding_4th_paper <- ggplot(diet_data, aes(x=factor(diet_treatment, level = diet
   xlab("Diet Treatment") + ylab("4th instar: Change in food (mg)")
 feeding_4th_paper
   
-feeding_5th_paper <-ggplot(diet_data, aes(x=factor(diet_treatment, level = diet_order), y=X5th_food_change)) +
+feeding_5th_paper <-ggplot(feeding_data, aes(x=factor(diet_treatment, level = diet_order), y=fifth_food_change)) +
   geom_boxplot(outlier.shape = NA) + 
   geom_jitter(width=0.13, alpha=0.5) +
   stat_summary(fun.y="mean", shape=18, size=1.2) +
@@ -181,11 +194,11 @@ ggarrange(size_graph_paper, feeding_4th_paper, feeding_5th_paper,
           hjust=-7.5, align="hv")
 
 #Figure S1
-size_regress<-ggplot(diet_data, aes(photo_mass, avg_percent))+ geom_point(alpha=0.7)+ 
+size_regress<-ggplot(diet_data, aes(photo_mass, melanin_percent))+ geom_point(alpha=0.7)+ 
   geom_smooth(method=lm, color="Black") + 
-  annotate(geom="text", x=4.5, y=96, label="r"^2~"=0.04 , p=0.002")+
+  annotate(geom="text", x=4.5, y=96, label="r"^2~"=0.04 , p=0.001")+
   theme_classic(base_size = 16) + theme(legend.position="none",text=element_text(family="Times New Roman")) +
-  xlab("Larval mass (g)") + ylab("Proportion melanic area (%)")+
+  xlab("Larval mass (g)") + ylab("Percent melanic area")+
   ylim(50, 100) 
 size_regress
 
